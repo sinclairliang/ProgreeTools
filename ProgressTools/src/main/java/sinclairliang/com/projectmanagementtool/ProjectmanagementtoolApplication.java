70b7413152fff6adc80995d9f71274911b4caede
Sinclair Liang <sinclairliang@gmail.com>
2018-12-16 22:32:35 -0800

    first commit

---
+++ ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/ProjectmanagementtoolApplication.java
@@ -0,0 +1,14 @@
+package sinclairliang.com.projectmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmanagementtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectmanagementtoolApplication.class, args);
+    }
+
+}
+
---
+++ ProgressTools/src/test/java/sinclairliang/com/projectmanagementtool/ProjectmanagementtoolApplicationTests.java
@@ -0,0 +1,17 @@
+package sinclairliang.com.projectmanagementtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectmanagementtoolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
