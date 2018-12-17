f9c0325d474294b42e1f5e14d4c8be0f38fe6897
Sinclair Liang <sinclairliang@gmail.com>
2018-12-16 23:06:04 -0800

    Project interface

---
+++ ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package sinclairliang.com.projectmanagementtool.repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import sinclairliang.com.projectmanagementtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
