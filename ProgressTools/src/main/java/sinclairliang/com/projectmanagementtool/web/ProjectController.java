c25e0f70d96472a7613e08cb6c6ce5eddf6753e7
Sinclair Liang <sinclairliang@gmail.com>
2018-12-16 23:59:22 -0800

    make sure users sumbit valid Project objects

--- ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/web/ProjectController.java
+++ ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/web/ProjectController.java
@@ -4,12 +4,15 @@ package sinclairliang.com.projectmanagementtool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sinclairliang.com.projectmanagementtool.domain.Project;
 import sinclairliang.com.projectmanagementtool.services.ProjectService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,7 +22,11 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
