ae897ff228c6d53fd242afc4afe8eafe74d5ad12
Sinclair Liang <sinclairliang@gmail.com>
2018-12-16 23:31:38 -0800

    our APIs are looking good

--- ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/web/ProjectController.java
+++ ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/web/ProjectController.java
@@ -1,4 +1,27 @@
 package sinclairliang.com.projectmanagementtool.web;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sinclairliang.com.projectmanagementtool.domain.Project;
+import sinclairliang.com.projectmanagementtool.services.ProjectService;
+
+@RestController
+@RequestMapping("/api/project")
 public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
 }
