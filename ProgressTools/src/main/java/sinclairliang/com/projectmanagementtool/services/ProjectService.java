86e05f868982b5bb4d1d793bc4d50e27c85c6842
Sinclair Liang <sinclairliang@gmail.com>
2018-12-17 17:02:47 -0800

    added exception handler for duplicated project identifier

--- ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/services/ProjectService.java
+++ ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/services/ProjectService.java
@@ -3,6 +3,7 @@ package sinclairliang.com.projectmanagementtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sinclairliang.com.projectmanagementtool.domain.Project;
+import sinclairliang.com.projectmanagementtool.exceptions.ProjectIdException;
 import sinclairliang.com.projectmanagementtool.repositories.ProjectRepository;
 
 @Service
@@ -13,7 +14,12 @@ public class ProjectService {
     public Project saveOrUpdateProject(Project project) {
 
         // many more logic
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            // set the Project Identifier to upper case;
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already existed");
+        }
     }
 }
