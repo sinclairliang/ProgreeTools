4db04e9e5d5724e56e7924c99ef29e3e25f041e5
Sinclair Liang <sinclairliang@gmail.com>
2018-12-17 17:37:27 -0800

    methods for finding project

--- ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/web/ProjectController.java
+++ ProgressTools/src/main/java/sinclairliang/com/projectmanagementtool/web/ProjectController.java
@@ -5,7 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,6 @@ import sinclairliang.com.projectmanagementtool.services.MapValidation;
 import sinclairliang.com.projectmanagementtool.services.ProjectService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -30,11 +29,15 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-
         ResponseEntity<?> errorMap = MapValidationService.MapValidationService(result);
         if (errorMap != null) return errorMap;
-
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+        Project project = projectService.findProjectByIdentifier(projectId.toUpperCase());
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 }
