1ef2d2f3fc35a264af4a3b5425183467053105a8
Sinclair Liang <sinclairliang@gmail.com>
2018-12-26 16:58:26 -0800

    added React Router

--- progresstool-react-client/src/App.js
+++ progresstool-react-client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
