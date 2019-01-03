c4117cd2747925b2878e98d84801a3e5696b9754
Sinclair Liang <sinclairliang@gmail.com>
2019-01-02 18:40:04 -0800

    working on redux

--- progresstool-react-client/src/store.js
+++ progresstool-react-client/src/store.js
@@ -1,4 +1,8 @@
-import {createStore, applyMiddleware, compose} from "redux";
+import {
+    createStore,
+    applyMiddleware,
+    compose
+} from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers"
 
@@ -9,9 +13,18 @@ const middleware = [thunk]
 let store;
 
 if (window.navigator.userAgent.includes("Chrome")) {
-    store = createStore(rootReducer, initalState, compose(applyMiddleware(...middleware)))
+    store = createStore(rootReducer, initalState, compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()));
 } else {
-    
+    store = createStore(
+        rootReducer,
+        initalState,
+        compose(
+            applyMiddleware(...middleware))
+    );
+
 }
 
 export default store;
