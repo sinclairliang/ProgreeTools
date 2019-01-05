23f6d301609ffcfe95fe3d45f4f91fffbb1b7553
Sinclair Liang <sinclairliang@gmail.com>
2019-01-04 18:21:29 -0800

    reading errors from redux store

--- progresstool-react-client/src/components/Project/AddProject.js
+++ progresstool-react-client/src/components/Project/AddProject.js
@@ -2,7 +2,7 @@ import React, {
   Component
 } from "react";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { createProject } from "../../actions/projectActions"
 
 class AddProject extends Component {
@@ -13,20 +13,29 @@ class AddProject extends Component {
       projectIdentifier: "",
       description: "",
       start_date: "",
-      end_date: ""
+      end_date: "",
+      errors: {}
     };
 
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+
+// life cycle hooks;
+componentWillReceiveProps(nextProps) {
+  if (nextProps.errors) {
+    this.setState({ errors: nextProps.errors });
+  }
+}
+
+
   onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   onSubmit(e) {
+    e.preventDefault();
     const newProject = {
       projectName: this.state.projectName,
       projectIdentifier: this.state.projectIdentifier,
@@ -34,10 +43,13 @@ class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date
     };
-    this.props.createProject(newProject, this.props.history)
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
+    const {errors} = this.state;
+
+    
     return (
       <div>
         <div className="project">
@@ -56,6 +68,7 @@ class AddProject extends Component {
                       value={this.state.projectName}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectName}</p>
                   </div>
                   <div className="form-group">
                     <input
@@ -67,6 +80,7 @@ class AddProject extends Component {
                       onChange={this.onChange}
                     />
                   </div>
+                  <p>{errors.projectIdentifier}</p>
                   <div className="form-group">
                     <textarea
                       className="form-control form-control-lg"
@@ -76,6 +90,7 @@ class AddProject extends Component {
                       onChange={this.onChange}
                     />
                   </div>
+                  <p>{errors.description}</p>
                   <h6>Start Date</h6>
                   <div className="form-group">
                     <input
@@ -85,7 +100,7 @@ class AddProject extends Component {
                       value={this.state.start_date}
                       onChange={this.onChange}
                     />
-                  </div>
+                  </div>                
                   <h6>Estimated End Date</h6>
                   <div className="form-group">
                     <input
@@ -111,11 +126,15 @@ class AddProject extends Component {
 }
 
 AddProject.prototypes = {
-  createProject: PropTypes.func.isRequired
+  createProject: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
 }
 
+const mapStateToProps = state => ({
+  errors: state.errors
+});
 
 export default connect(
-  null, 
+  mapStateToProps, 
   { createProject }
   ) (AddProject);
