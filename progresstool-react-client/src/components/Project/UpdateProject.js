ad7adae0566389619f048bd743f4b0bffc3c01bf
Sinclair Liang <sinclairliang@gmail.com>
2019-01-21 23:57:58 -0800

    working on update project

---
+++ progresstool-react-client/src/components/Project/UpdateProject.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class UpdateProject extends Component {
+    render () {
+        return (
+            <h1>Update Project</h1>
+        );
+    }
+}
