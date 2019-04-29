23b7f0c4be7e4d1f6a2a77e08a603f4f5189c234
Sinclair Liang <sinclairliang@gmail.com>
2019-04-29 01:20:57 -0700

    redux set up

--- progresstool-react-client/src/App.js
+++ progresstool-react-client/src/App.js
@@ -5,17 +5,21 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/addProject" component={AddProject} />
-          <Route exact path="/dashboard" component={Dashboard} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
